efadf85d133ebbe9d0368ae1022077d5257a1678
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2023-09-01 16:07:24 -0700

    Styled item card

--- components/item.js
+++ components/item.js
@@ -1,35 +1,158 @@
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import CardOverflow from '@mui/joy/CardOverflow';
-import Typography from '@mui/joy/Typography';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Button from '@mui/joy/Button';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import Avatar from "@mui/joy/Avatar";
 
-export default function Item ({data}) {
-    if (data.type === "announcement") {
-        return (
-            <Card variant='' >
-                <CardContent orientation="horizontal">
-                    <ChatBubbleIcon />
-                    <Typography level="body-sm">
-                            {data.text}
-                    </Typography>
-                </CardContent>
-            </Card>
-        )
-    } else if (data.type === "assignment") {
-        return (
-            <Card variant=''>
-                <CardContent orientation="horizontal">
-                    <AssignmentIcon />
-                    <Typography level="body-sm">
-                        {data.description}
-                    </Typography>
-                </CardContent>
-            </Card>
-        )
-    } else {
-    }
+export default function Item({ data }) {
+  if (data.type === "announcement") {
+    return (
+      <Card
+        style={{
+          width: "400px",
+          backgroundColor: "#fafafa",
+          border: "1px solid #e0e0e0",
+          borderRadius: "8px",
+          padding: "26px",
+          marginBottom: "20px",
+          paddingBottom: "30px",
+          maxheight: "500px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginBottom: "10px",
+          }}
+        >
+          <Typography
+            level="body-sm"
+            style={{ fontSize: "1.2em", fontWeight: "600" }}
+          >
+            {data.title}
+          </Typography>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Avatar
+              style={{
+                backgroundColor: "#00796B",
+                color: "#fff",
+                marginRight: "8px",
+              }}
+            >
+              {data.author[0]}{" "}
+              {/* Assuming the first letter of the author's name */}
+            </Avatar>
+            <Typography
+              level="body-sm"
+              style={{ fontSize: "1em", color: "#757575" }}
+            >
+              {data.timestamp}
+            </Typography>
+          </div>
+        </div>
+        <CardContent
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "flex-start",
+          }}
+        >
+          <ChatBubbleIcon
+            style={{
+              color: "maroon",
+              marginRight: "16px",
+              fontSize: "3em",
+              backgroundColor: "#e0e0e0",
+              borderRadius: "50%",
+              padding: "12px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          />
+          <Typography
+            level="body-sm"
+            style={{ fontSize: "0.9em", fontWeight: "500" }}
+          >
+            {data.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  } else if (data.type === "assignment") {
+    return (
+      <Card
+        style={{
+          width: "400px",
+          backgroundColor: "#fafafa",
+          border: "1px solid #e0e0e0",
+          borderRadius: "8px",
+          padding: "16px",
+          marginBottom: "20px",
+        }}
+      >
+        <CardContent
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "flex-start",
+          }}
+        >
+          <AssignmentIcon
+            style={{
+              color: "#ef6c00",
+              marginRight: "16px",
+              fontSize: "3em",
+              backgroundColor: "#e0e0e0",
+              borderRadius: "50%",
+              padding: "12px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          />
+          <Typography
+            level="body-sm"
+            style={{ fontSize: "1.2em", fontWeight: "500" }}
+          >
+            {data.description}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  } else {
+    return (
+      <Card
+        style={{
+          maxWidth: "345px",
+          backgroundColor: "#fafafa",
+          border: "1px solid #e0e0e0",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+        }}
+      >
+        <CardContent
+          style={{
+            display: "flex",
+            flexDirection: "horizontal",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            level="body-sm"
+            style={{ fontSize: "1rem", marginBottom: "1rem" }}
+          >
+            It doesn't work
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,64 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Item from '@/components/item'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Item from "@/components/item";
 
 export default function Home() {
+  const announcement = {
+    type: "announcement",
+    title: "title of lorem",
+    author: "Lorem Ipsum",
+    timestamp: "12:00 PM",
+    courseId: "1234134",
+    id: "133423",
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    // materials: [],
+    state: "PUBLISHED",
+    alternateLink: "",
+    creationTime: "string",
+    updateTime: "string",
+    scheduledTime: "string",
+    assigneeMode: "ALL_STUDENTS",
+    // individualStudentsOptions: {
+    //   {
+    //     studentIds: ["3938"]
+    //   }
+    // },
+    creatorUserId: "string",
+  };
 
-  const announcement =
-    {
-      type: "announcement",
-      courseId: "1234134",
-      id: "133423",
-      text: "dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text",
-      // materials: [],
-      state: "PUBLISHED",
-      alternateLink: "",
-      creationTime: "string",
-      updateTime: "string",
-      scheduledTime: "string",
-      assigneeMode: "ALL_STUDENTS",
-      // individualStudentsOptions: {
-      //   {
-      //     studentIds: ["3938"]
-      //   }
-      // },
-      creatorUserId: "string"
-    }
-
-    const assignment =
-    {
-      type: "assignment",
-      courseId: "string",
-      id: "string",
-      title: "string",
-      description: "more dummy text",
-      materials: [],
-      //state: enum (CourseWorkState),
-      alternateLink: "string",
-      creationTime: "string",
-      updateTime: "string",
-      //dueDate: {
-      //  object (Date)
-      //},
-      //dueTime: {
-      //  object (TimeOfDay)
-      //},
-      scheduledTime: "string",
-      maxPoints: 0,
-      //workType: enum (CourseWorkType),
-      associatedWithDeveloper: false,
-      //assigneeMode: enum (AssigneeMode),
-      //individualStudentsOptions: {
-      //  object (IndividualStudentsOptions)
-      //},
-      // submissionModificationMode: enum (SubmissionModificationMode),
-      creatorUserId: "string",
-      topicId: "string"//,
-      //gradeCategory: {
-      //  object (GradeCategory)
-      }
+  const assignment = {
+    type: "assignment",
+    courseId: "string",
+    id: "string",
+    title: "string",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    materials: [],
+    //state: enum (CourseWorkState),
+    alternateLink: "string",
+    creationTime: "string",
+    updateTime: "string",
+    //dueDate: {
+    //  object (Date)
+    //},
+    //dueTime: {
+    //  object (TimeOfDay)
+    //},
+    scheduledTime: "string",
+    maxPoints: 0,
+    //workType: enum (CourseWorkType),
+    associatedWithDeveloper: false,
+    //assigneeMode: enum (AssigneeMode),
+    //individualStudentsOptions: {
+    //  object (IndividualStudentsOptions)
+    //},
+    // submissionModificationMode: enum (SubmissionModificationMode),
+    creatorUserId: "string",
+    topicId: "string", //,
+    //gradeCategory: {
+    //  object (GradeCategory)
+  };
   return (
     <>
       <Head>
@@ -66,10 +67,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Item data={assignment}/> 
-        <Item data={announcement}/>
+      <main
+        style={{
+          // background: "red",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        <Item data={assignment} />
+        <Item data={announcement} />
       </main>
     </>
-  )
+  );
 }

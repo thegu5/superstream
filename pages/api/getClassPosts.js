9be4e56a3704859b3b8533ac00b0bd54a1255f0d
David Chen <dchen07@protonmail.com>
2023-09-02 13:02:46 -0400

    Revise cache timing

--- pages/api/getClassPosts.js
+++ pages/api/getClassPosts.js
@@ -170,7 +170,7 @@ export default async function handler(req, res) {
     // enable caching
     res.setHeader(
       "Cache-Control",
-      "private, s-maxage=300, stale-while-revalidate=500",
+      "private, s-maxage=10, stale-while-revalidate=20",
     );
     res.status(200).json(posts);
   } catch (err) {

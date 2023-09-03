106a329a815ebf02bde9f03ce66ab7033ef3a70f
David Chen <dchen07@protonmail.com>
2023-09-02 23:33:28 -0400

    Add userInfo API route
    Allows for frontend to fetch the logged in user's information for
    display.

---
+++ pages/api/userInfo.js
@@ -0,0 +1,11 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+const tokenfile = fs.readFileSync('token.json');
+
+export default async function handler(req, res) {
+    const classroom = google.classroom({ version: 'v1', auth: google.auth.fromJSON(JSON.parse(tokenfile))});
+    const response = await classroom.userProfiles.get({
+        userId: 'me'
+    })
+    res.status(200).json(response.data);
+}
